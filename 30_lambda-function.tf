a78a133213a3fd69e72f6da9bda61aca3dfd4477
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-12-06 22:25:27 +0900

    [Remove] 不要なattribute: timeout

--- 30_lambda-function.tf
+++ 30_lambda-function.tf
@@ -4,7 +4,6 @@ resource "aws_lambda_function" "env_test" {
 	role = "${aws_iam_role.iam_for_lambda.arn}"
 	runtime = "nodejs4.3"
 	handler = "index.handler"
-	timeout = 10
 	filename = "index.zip"
 	source_code_hash = "${base64sha256(file("index.zip"))}"
 }
